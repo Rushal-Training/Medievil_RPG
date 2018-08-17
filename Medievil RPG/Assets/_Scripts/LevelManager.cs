5ac0e21eaddf03d35e477d0fea9130da52de3b49
Rushal <TheRushal@gmail.com>
2018-08-17 15:59:59 -0500

    Initial commit

---
+++ Medievil RPG/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	[SerializeField] float autoLoadNextLevelAfter = 0;
+
+	public void LoadLevel( string name )
+	{
+		SceneManager.LoadScene( name );
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex + 1 );
+	}
+
+	public void QuitRequest()
+	{
+		Application.Quit();
+	}
+
+	void Start()
+	{
+		if( autoLoadNextLevelAfter != 0 )
+		{
+			Invoke( "LoadNextLevel", autoLoadNextLevelAfter );
+		}		
+	}
+}
---
+++ Medievil RPG/Assets/_Scripts/MusicLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoader : MonoBehaviour
+{
+	[SerializeField] AudioClip[] levelMusic;
+
+	AudioSource audioSource;
+
+	void Awake()
+	{
+		DontDestroyOnLoad( gameObject );
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	void OnLevelWasLoaded( int level )
+	{
+		if ( levelMusic [level] )
+		{
+			audioSource.clip = levelMusic [level];
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
