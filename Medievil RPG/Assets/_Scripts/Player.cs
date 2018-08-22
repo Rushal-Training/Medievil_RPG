3556809d360d9c8c403fe75aaf2b917f75c25f30
Rushal <TheRushal@gmail.com>
2018-08-21 22:05:07 -0500

    climbing working, still messed up when at the top

--- Medievil RPG/Assets/_Scripts/Player.cs
+++ Medievil RPG/Assets/_Scripts/Player.cs
@@ -5,9 +5,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField] float runSpeed = 3f;
-	[SerializeField] float jumpSpeed = 3f;
+	[SerializeField] float climbSpeed = 3f;
+	[SerializeField] float jumpSpeed = 16f;
+	[SerializeField] float runSpeed = 4f;
 
+	float startingGravity;
+	float startingRunSpeed;
 	bool isAlive = true;
 
 	Animator animator;
@@ -19,23 +22,72 @@ public class Player : MonoBehaviour
 		animator = GetComponent<Animator>();
 		myCollider2D = GetComponent<Collider2D>();
 		myRigidbody = GetComponent<Rigidbody2D>();
+
+		startingGravity = myRigidbody.gravityScale;
+		startingRunSpeed = runSpeed;
 	}
 	
 	void Update ()
 	{
-		Run();
 		FlipSprite();
+		Climb();
+		Run();
 		Jump();
+	}
+
+	void FlipSprite()
+	{
+		bool playerHasHorizontalSpeed = Mathf.Abs( myRigidbody.velocity.x ) > Mathf.Epsilon;
+		if ( playerHasHorizontalSpeed )
+		{
+			transform.localScale = new Vector2( Mathf.Sign( myRigidbody.velocity.x ), 1f );
+		}
 	}
 
 	void Run()
 	{
 		float controlThrow = CrossPlatformInputManager.GetAxis( "Horizontal" );
-		Vector2 playerVelocity = new Vector2( controlThrow * runSpeed, myRigidbody.velocity.y );
-		myRigidbody.velocity = playerVelocity;
+		Vector2 runVelocity = new Vector2( controlThrow * runSpeed, myRigidbody.velocity.y );
+		myRigidbody.velocity = runVelocity;
 
 		bool playerHasHorizontalSpeed = Mathf.Abs( myRigidbody.velocity.x ) > Mathf.Epsilon;
 		animator.SetBool( "Running", playerHasHorizontalSpeed );
+	}
+
+	void Climb()
+	{
+		if ( !myCollider2D.IsTouchingLayers( LayerMask.GetMask( "Climbing" ) ) )
+		{
+			myRigidbody.gravityScale = startingGravity;
+			runSpeed = startingRunSpeed;
+
+			animator.SetBool( "ClimbingIdle", false );
+			animator.SetBool( "Climbing", false );
+			return;
+		}
+
+		animator.SetBool( "ClimbingIdle", true );
+
+		float controlThrow = CrossPlatformInputManager.GetAxis( "Vertical" );
+		Vector2 climbVelocity = new Vector2( myRigidbody.velocity.x, controlThrow * climbSpeed );
+		myRigidbody.velocity = climbVelocity;
+
+		bool horizSpeed = Mathf.Abs( myRigidbody.velocity.x ) > Mathf.Epsilon;
+		bool playerHasVerticalSpeed = Mathf.Abs( myRigidbody.velocity.y ) > Mathf.Epsilon;
+		if ( playerHasVerticalSpeed || horizSpeed )
+		{
+			runSpeed = 2f;
+			animator.SetBool( "ClimbingIdle", false );
+			animator.SetBool( "Climbing", true );
+			myRigidbody.gravityScale = 0;
+		}
+		else
+		{
+			runSpeed = 2f;
+			animator.SetBool( "ClimbingIdle", true );
+			animator.SetBool( "Climbing", false );
+			myRigidbody.gravityScale = 0;
+		}
 	}
 
 	void Jump()
@@ -54,7 +106,7 @@ public class Player : MonoBehaviour
 
 		// If not touching the Ground layer, exit
 		// Otherwise set landing animation bool
-		if ( !myCollider2D.IsTouchingLayers( LayerMask.GetMask( "Ground" ))) { return; }
+		if ( !myCollider2D.IsTouchingLayers( LayerMask.GetMask( "Ground" ) ) ) { return; }
 
 		animator.ResetTrigger( "Jumping" );
 
@@ -66,12 +118,4 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	void FlipSprite()
-	{
-		bool playerHasHorizontalSpeed = Mathf.Abs( myRigidbody.velocity.x ) > Mathf.Epsilon;
-		if ( playerHasHorizontalSpeed )
-		{
-			transform.localScale = new Vector2( Mathf.Sign( myRigidbody.velocity.x ), 1f);
-		}
-	}
 }
