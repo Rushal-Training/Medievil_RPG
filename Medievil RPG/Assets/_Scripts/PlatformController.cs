68d37566457189aaecb19cafa4a9a33a94444706
Rushal <TheRushal@gmail.com>
2018-08-26 16:10:14 -0500

    Platform waypoints and easing

--- Medievil RPG/Assets/_Scripts/PlatformController.cs
+++ Medievil RPG/Assets/_Scripts/PlatformController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlatformController : RaycastController
 {
+	[SerializeField] [Range(0,2)] float easeAmount;
+	[SerializeField] float speed;
+	[SerializeField] float waitTime;
+	[SerializeField] bool cyclic;
 	[SerializeField] LayerMask passengerMask;
-	[SerializeField] Vector3 move;
+	[SerializeField] Vector3[] localWaypoints;
 
-	List<PassengerMovement> passengerMovement;
 	Dictionary<Transform, Controller2D> passengerDictionary = new Dictionary<Transform, Controller2D>();
+	List<PassengerMovement> passengerMovement;
+	Vector3[] globalWaypoints;
+
+	int fromWaypointIndex;
+	float percentBetweenWaypoints;
+	float nextMoveTime;
 
 	struct PassengerMovement
 	{
@@ -28,13 +37,19 @@ public class PlatformController : RaycastController
 	protected override void Start()
 	{
 		base.Start();
+
+		globalWaypoints = new Vector3[localWaypoints.Length];
+		for ( int i = 0; i < localWaypoints.Length; i++ )
+		{
+			globalWaypoints[i] = localWaypoints[i] + transform.position;
+		}
 	}
 
 	void Update ()
 	{
 		UpdateRaycastOrigins();
 
-		Vector3 velocity = move * Time.deltaTime;
+		Vector3 velocity = CalculatePlatformMovement();
 
 		CalculatePassengerMovement( velocity );
 
@@ -137,5 +152,64 @@ public class PlatformController : RaycastController
 			}
 		}
 
+	}
+
+	float Ease(float x)
+	{
+		float a = easeAmount + 1;
+		return Mathf.Pow( x, a ) / (Mathf.Pow( x, a ) + Mathf.Pow( 1 - x, a ));
+	}
+
+	Vector3 CalculatePlatformMovement()
+	{
+		if ( Time.time < nextMoveTime )
+		{
+			return Vector3.zero;
+		}
+
+		fromWaypointIndex %= globalWaypoints.Length;
+		int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+		float distanceBetweenWaypoints = Vector3.Distance( globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex] );
+
+		percentBetweenWaypoints += Time.deltaTime * speed/distanceBetweenWaypoints;
+		percentBetweenWaypoints = Mathf.Clamp01( percentBetweenWaypoints );
+
+		float easedPercentBetweenWaypoints = Ease( percentBetweenWaypoints );
+
+		Vector3 newPos = Vector3.Lerp( globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], easedPercentBetweenWaypoints );
+
+		if( percentBetweenWaypoints >= 1 )
+		{
+			percentBetweenWaypoints = 0;
+			fromWaypointIndex++;
+
+			if ( !cyclic )
+			{
+				if ( fromWaypointIndex >= globalWaypoints.Length - 1 )
+				{
+					fromWaypointIndex = 0;
+					System.Array.Reverse( globalWaypoints );
+				}
+			}
+			nextMoveTime = Time.time + waitTime;
+		}
+
+		return newPos - transform.position;
+	}
+
+	void OnDrawGizmos()
+	{
+		if ( localWaypoints != null )
+		{
+			float size = 0.3f;
+			Gizmos.color = Color.red;
+
+			for(int i = 0; i < localWaypoints.Length; i++ )
+			{
+				Vector3 globalWaypointPos = ( Application.isPlaying ) ? globalWaypoints[i] : localWaypoints[i] + transform.position;
+				Gizmos.DrawLine( globalWaypointPos - Vector3.up * size, globalWaypointPos + Vector3.up * size );
+				Gizmos.DrawLine( globalWaypointPos - Vector3.left * size, globalWaypointPos + Vector3.left * size );
+			}
+		}
 	}
 }
